b6fa0ac2da49e950a5248b632669936eca5619e9
karlsurmacz <karlsurmacz@gmail.com>
2016-03-13 19:37:09 +0000

    Added cross-entropy function.

--- softmax.py
+++ softmax.py
@@ -11,6 +11,10 @@ def softmax(vector):
     output = np.exp(vector)/np.sum(np.exp(vector))
     return output
 
+def cross_entropy(S,L):
+    D = -np.dot(S, np.log(L))
+    return D
+
 if __name__ == "__main__":
     scores = [3, 2, 0.1]
     print(softmax(scores))
@@ -19,3 +23,4 @@ if __name__ == "__main__":
     scores = np.vstack([x, np.ones_like(x), 0.2*np.ones_like(x)])
     plt.plot(x, softmax(scores).T, linewidth=2)
     plt.show
+    
